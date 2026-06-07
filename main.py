13324295ae331de97ac183a1606a319f19caccc7
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-08 04:48:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,22 @@ import requests
 import streamlit as st
 st.title("AI Implementation")
 st.write("AI implementation")
-def ask_ai(question):
 
-    api_key = st.secrets["Chat_bot"]
-
-    headers = {
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
+def Ask_friend():
+    api_url="https://openrouter.ai/api/v1/chat/completions"
+    api_key=st.secrets["Chat_bot"]
+    headers={
+        "Authorization":f"Bearer {api_key}",
+        "Content-Type":"application/json"
     }
-
-    payload = {
-        "model": "openrouter/free",
-        "messages": [{
-                "role": "user",
-                "content": question
-            }
-        ]
+    payload={
+       "model":"openrouter/free",
+       "message":[{"role":"system","content":"You are Shweta.you are expert interviewer and ask questions related to programming only"},
+                  {"role":"user","content":Question}]
     }
-
-    response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
-        headers=headers,
-        json=payload
-    )
-
-    result = response.json()
-
+    response=requests.post(api_url,headers=headers,json=payload)
+    result=response.json()
     return result["choices"][0]["message"]["content"]
-
-
-question = st.text_input("Ask Question: ")
-
-answer = ask_ai(question)
+Question=st.text_input("",values="Ask Shweta")
+answer=Ask_friend()
 st.markdown(answer)
