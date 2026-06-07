bb99751ef57d0f1d375af664179ff889cbc28ee5
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-07 13:43:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,20 @@ import requests
 import streamlit as st
 st.title("AI Implementation")
 st.write("AI implementation")
-def ask_ai(question, stream):
+def ask_ai(question):
 
-    api_key = st.secrets[
-        "OPENROUTER_API_KEY"
-    ]
+    api_key = st.secrets["Chat_bot"]
 
     headers = {
-        "Authorization":
-        f"Bearer {api_key}",
-
-        "Content-Type":
-        "application/json"
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
     }
 
     payload = {
-
-        "model":
-        "openrouter/free",
-
-        "messages": [
-
-            {
-                "role": "system",
-
-                "content": f"""
-You are Margdarshak AI.
-
-Student Recommended Stream:
-{stream}
-
-You are an expert career counselor.
-
-Provide:
-
-1. Career guidance
-2. Career roadmap
-3. Required skills
-4. Future opportunities
-5. Exam suggestions
-6. College suggestions
-
-Keep answers practical and student friendly.
-"""
-            },
-
-            {
+        "model": "openrouter/free",
+        "messages": [{
                 "role": "user",
-
-                "content": f"""
-Recommended Stream:
-{stream}
-
-Question:
-{question}
-"""
+                "content": question
             }
         ]
     }
@@ -64,16 +23,15 @@ Question:
     response = requests.post(
         "https://openrouter.ai/api/v1/chat/completions",
         headers=headers,
-        json=payload,
-        timeout=60
+        json=payload
     )
 
-    data = response.json()
+    result = response.json()
 
-    return data[
-        "choices"
-    ][0][
-        "message"
-    ][
-        "content"
-  ]
+    return result["choices"][0]["message"]["content"]
+
+
+question = st.input_text("Ask Question: ")
+
+answer = ask_ai(question)
+st.markdown(answer)
